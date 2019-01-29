0dc6b6c1f1674c6121d5d6223d7b924b739438e6
MihaiBojescu <bojescu.mihai@gmail.com>
2019-01-29 17:24:18 +0200

    Changed the response message for 404 on /users?id=...

--- microservices/users/src/get/userById.js
+++ microservices/users/src/get/userById.js
@@ -7,7 +7,7 @@ const getUserById = async (req, res) => {
     const user = await User.findById(req.query.id)
     res.status(200).json({ data: user })
   } catch (error) {
-    res.status(404).json({ data: {} })
+    res.status(404).json({ error })
   }
 }
 
