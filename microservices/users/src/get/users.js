3f4e9be4a836013846d44b2e50862eab410b8b4a
IoanaVas <ioana.dvasiliu@yahoo.com>
2019-01-29 17:25:06 +0200

    fixed getUser

--- microservices/users/src/get/users.js
+++ microservices/users/src/get/users.js
@@ -6,8 +6,8 @@ const getUsers = async (req, res) => {
   try {
     const users = await User.find({})
     res.status(200).json({ data: users })
-  } catch (err) {
-    res.status(404).json({ data: err })
+  } catch (error) {
+    res.status(404).json({ error })
   }
 }
 
