3bfd9a24c7ab2334382bca66a5659515e48cb981
IoanaVas <ioana.dvasiliu@yahoo.com>
2019-01-29 17:24:17 +0200

    fixed getUser

--- microservices/users/src/get/users.js
+++ microservices/users/src/get/users.js
@@ -3,8 +3,12 @@
 const { User } = require('../models')
 
 const getUsers = async (req, res) => {
-  const users = await User.find({})
-  res.status(200).json({ data: users })
+  try {
+    const users = await User.find({})
+    res.status(200).json({ data: users })
+  } catch (err) {
+    res.status(404).json({ data: err })
+  }
 }
 
 module.exports = {
