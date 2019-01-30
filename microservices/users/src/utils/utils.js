58a4d7568c142fd0c416676e7db4ec74f4db8477
MihaiBojescu <bojescu.mihai@gmail.com>
2019-01-30 16:39:39 +0200

    Changed client password regex

--- microservices/users/src/utils/utils.js
+++ microservices/users/src/utils/utils.js
@@ -2,7 +2,7 @@
 
 const emailRegex = /^[a-zA-Z0-9]+([_.-][a-zA-Z0-9]+)*[@][a-z]+[.][a-z]+$/
 const databasePasswordRegex = /^[a-z0-9]{64}$/
-const clientPasswordRegex = /^[a-zA-Z0-9._]{8}$/
+const clientPasswordRegex = /^[a-zA-Z0-9._]{8,}$/
 
 exports.default = {
   emailRegex,
