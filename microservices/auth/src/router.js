45a861a498a1098c7bee8213843197e059563894
IoanaVas <ioana.dvasiliu@yahoo.com>
2019-01-31 14:38:46 +0200

    fixed route

--- microservices/auth/src/router.js
+++ microservices/auth/src/router.js
@@ -6,18 +6,7 @@ const { postSession, deleteSession } = require('./methods').default
 
 const router = Router()
 
-const routeByQueryParameter = list =>
-  (req, res) =>
-    list.find(item =>
-      item.params.every(parameter => req.query[parameter])
-    ).action(req, res)
-
-router.post('/', routeByQueryParameter([
-  { params: [], action: postSession }
-]))
-
-router.delete('/', routeByQueryParameter([
-  { params: [], action: deleteSession }
-]))
+router.post('/', postSession)
+router.delete('/', deleteSession)
 
 exports.default = router
