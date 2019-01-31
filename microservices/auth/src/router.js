b9dc6e7247d771ef849eb20da344e9e32b07ae00
IoanaVas <ioana.dvasiliu@yahoo.com>
2019-01-31 15:05:38 +0200

    updated router file for auth

--- microservices/auth/src/router.js
+++ microservices/auth/src/router.js
@@ -6,14 +6,6 @@ const { postSession } = require('./methods').default
 
 const router = Router()
 
-const routeByQueryParameter = list =>
-  (req, res) =>
-    list.find(item =>
-      item.params.every(parameter => req.query[parameter])
-    ).action(req, res)
-
-router.post('/', routeByQueryParameter([
-  { params: [], action: postSession }
-]))
+router.post('/', postSession)
 
 exports.default = router
