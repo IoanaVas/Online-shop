8a931effa3e12e06b71d35bb3949e6c77945f8cd
IoanaVas <ioana.dvasiliu@yahoo.com>
2019-02-18 14:45:54 +0200

    added post method for media

--- src/services/products/media/post.js
+++ src/services/products/media/post.js
@@ -1,8 +1,10 @@
 'use strict'
 
-const aws = require('aws-sdk')
+const AWS = require('aws-sdk')
+const crypto = require('crypto')
 
 const { Product } = require('../../../database/models').default
+const { extensionRegex, validate } = require('../../../utils').default
 const {
   SECRET_ACCESS_KEY,
   ACCESS_KEY_ID,
@@ -10,95 +12,158 @@ const {
   S3_BUCKET_NAME
 } = require('../../../../secrets.json')
 
-aws.config.update({
-  secretAccessKey: SECRET_ACCESS_KEY,
-  accessKeyId: ACCESS_KEY_ID,
+const s3 = new AWS.S3({
+  credentials: new AWS.Credentials({
+    accessKeyId: ACCESS_KEY_ID,
+    secretAccessKey: SECRET_ACCESS_KEY
+  }),
   region: AWS_REGION
 })
 
-const s3 = new aws.S3()
+// let params = {
+//   Bucket: S3_BUCKET_NAME,
+//   Key: 'updatedMedia'
+// }
 
-let params = {
-  Bucket: S3_BUCKET_NAME,
-  Key: 'updatedMedia'
-}
+const createMultipartUpload = async (s3, fileName) => {
+  const extension = fileName.match(extensionRegex)[0]
+  const params = {
+    Bucket: S3_BUCKET_NAME,
+    Key: crypto
+      .createHash('sha256')
+      .update(fileName)
+      .digest('hex') + extension
+  }
 
-let multipartMap = { Parts: [] }
-let a = 0
-
-const createMultipartUpload = async (s3, params) => {
   try {
     const result = await s3.createMultipartUpload(params).promise()
 
     return result
   } catch (error) {
     console.error(error)
+
+    throw error
   }
 }
 
-const uploadPart = async (s3, params) => {
+const uploadPart = (s3, params, index) => {
+  return s3.uploadPart(params).promise()
+    .then(eTag => {
+      eTag.PartNumber = index
+      return eTag
+    })
+}
+
+const completeMultipartUpload = async (s3, params) => {
   try {
-    const result = await s3.uploadPart(params).promise()
-    multipartMap.Parts[a].ETag = result.ETag
-    a++
+    const result = await s3.completeMultipartUpload(params).promise()
 
     return result
   } catch (error) {
     console.error(error)
+
+    throw error
   }
-}
-
-const completeMultipartUpload = async (s3, params) => {
-  const result = await s3.completeMultipartUpload(params).promise()
-
-  return result
 }
 
 const action = async (req, res) => {
   const { id } = req.params
-  let bigChunk = ''
-  let index = 0
+  const fileName = req.headers['x-file-name']
+  const error = validate('File name', fileName, extensionRegex)
 
-  const createdMultipartUpload = await createMultipartUpload(s3, params)
+  let bigChunk = Buffer.alloc(0)
+  let index = 1
+  let multipartMap = { Parts: [] }
 
-  req.on('data', async (chunk) => {
-    if (bigChunk.length < 1024 * 1024 * 5) {
-      bigChunk += chunk
-    } else {
-      index++
+  // if (error) {
+  //   res.status(400).json({ error: 'File name not provided' })
+  //   return
+  // }
 
-      const updatePartParams = {
-        ...createdMultipartUpload,
-        Body: bigChunk.slice(),
-        PartNumber: index
+  try {
+    if (!(await Product.findById(id))) {
+      return
+    }
+    const createdMultipartUpload = await createMultipartUpload(s3, fileName)
+
+    req.on('data', async (chunk) => {
+      if (bigChunk.length < 1024 * 1024 * 5) {
+        bigChunk = Buffer.concat([bigChunk, chunk])
+      } else {
+        const updatePartParams = {
+          ...createdMultipartUpload,
+          Body: bigChunk.slice(),
+          PartNumber: index
+        }
+
+        // res.write(`${index}`)
+
+        try {
+          const eTag = uploadPart(s3, updatePartParams, index)
+          multipartMap.Parts.push(eTag)
+          index++
+        } catch (error) {
+          console.error(error)
+
+          throw error
+        }
+
+        bigChunk = Buffer.alloc(0)
       }
+    })
 
-      bigChunk = Buffer.alloc(0)
+    req.on('end', async () => {
+      if (bigChunk.length !== 0) {
+        const updatePartParams = {
+          ...createdMultipartUpload,
+          Body: bigChunk,
+          PartNumber: index
+        }
 
-      multipartMap.Parts.push({
-        PartNumber: index
-      })
+        try {
+          const eTag = uploadPart(s3, updatePartParams, index)
+          multipartMap.Parts.push(eTag)
+        } catch (error) {
+          console.error(error)
 
-      await uploadPart(s3, updatePartParams)
-      if (a === multipartMap.Parts.length) {
-        console.log(a, multipartMap.Parts.length)
-        const completeUploadParams = {
-          ...params,
+          throw error
+        }
+
+        let completeUploadParams = {
+          Bucket: createdMultipartUpload.Bucket,
+          Key: createdMultipartUpload.Key,
           UploadId: createdMultipartUpload.UploadId,
           MultipartUpload: multipartMap
         }
-        console.log('params', completeUploadParams.MultipartUpload)
 
         try {
-          const result = await completeMultipartUpload(s3, completeUploadParams)
-
-          console.log(result)
+          completeUploadParams.MultipartUpload.Parts = await Promise.all(multipartMap.Parts)
         } catch (error) {
           console.error(error)
+
+          throw error
+        }
+
+        const completedUpload = await completeMultipartUpload(s3, completeUploadParams)
+
+        try {
+          const result = await Product.findOneAndUpdate({ _id: id }, { $push: { media: completedUpload.Location } })
+
+          res.status(200).json({ data: result })
+        } catch (error) {
+          throw error
         }
       }
+    })
+  } catch (error) {
+    if (error.name === 'CastError') {
+      res.status(404).json({ error: 'Invalid product ID' })
+    } else {
+      console.error(error)
+      res.status(500).json({ error: 'Something went wrong...' })
     }
-  })
+    return error
+  }
 }
 
 exports.default = action
